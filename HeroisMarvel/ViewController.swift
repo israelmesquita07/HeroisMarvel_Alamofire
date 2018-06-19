d1368d9246b6bd10ff772c8a76b21cb8fe5964e3
Israel3D <Israel3D@macbook-pro-de-israel.local>
2018-06-18 21:53:06 -0300

    Teste commit 3

--- HeroisMarvel/ViewController.swift
+++ HeroisMarvel/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Teste commit")
     }
     
     override func viewWillAppear(_ animated: Bool) {
